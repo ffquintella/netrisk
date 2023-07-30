98276e828e98ca7c2d3613cba2336ff0b8de41de
Felipe F. Quintella <felipe@quintella.com>
2023-07-30 13:05:48 -0300

    INC

--- src/Model/DTO/FileListing.cs
+++ src/Model/DTO/FileListing.cs
@@ -2,9 +2,9 @@
 
 public class FileListing
 {
-    public string Name { get; set; }
-    public string UniqueName { get; set; }
-    public string Type { get; set; }
+    public string Name { get; set; } = "";
+    public string UniqueName { get; set; } = "";
+    public string Type { get; set; } = "";
     public DateTime Timestamp { get; set; }
     public int OwnerId { get; set; }
 }
