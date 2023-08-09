c94fba01afb30b07650c9aa4d70472bf627c441e
Felipe F Quiontella <felipe@quintella.com>
2023-08-09 15:01:32 -0300

    INC

--- src/GUIClient/Views/EntityForm.axaml.cs
+++ src/GUIClient/Views/EntityForm.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.LogicalTree;
 using Avalonia.Markup.Xaml;
 using DAL.Entities;
 using Model.Entities;
@@ -11,7 +12,24 @@ public partial class EntityForm : UserControl
 
     public EntityForm(Entity entity, EntitiesConfiguration configuration): this()
     {
-        
+        var definition = configuration.Definitions[entity.DefinitionName];
+        CreateForm(entity, definition);
+    }
+    
+    private void CreateForm(Entity entity, EntityDefinition definition)
+    {
+        var form = new StackPanel();
+        foreach (var (key, value) in definition.Properties)
+        {
+            var label = new TextBlock {Text = key};
+            var textBox = new TextBox();
+            form.Children.Add(label);
+            form.Children.Add(textBox);
+        }
+
+        this.Content = form;
+        //var uc = this.GetControl<UserControl>("EntityForm");
+        //uc.Content = form;
     }
     
     public EntityForm()
