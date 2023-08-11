18351b7a0e0ae2dbda7aed0ab9dc90a2d41e2930
Felipe F Quintella <felipe@quintella.com>
2023-08-11 19:24:53 -0300

    INC

--- src/AvaloniaExtraControls/MultiSelect/MultiSelect.axaml.cs
+++ src/AvaloniaExtraControls/MultiSelect/MultiSelect.axaml.cs
@@ -9,12 +9,21 @@ namespace AvaloniaExtraControls.MultiSelect;
 public class MultiSelect : TemplatedControl
 {
     public static readonly StyledProperty<IBrush?> BackgroundProperty =
-        Border.BackgroundProperty.AddOwner<MultiSelect>();
+        AvaloniaProperty.Register<MultiSelect, IBrush?>(nameof(Background));
 
     public IBrush? Background
     {
         get { return GetValue(BackgroundProperty); }
         set { SetValue(BackgroundProperty, value); }
+    }
+
+    public static readonly StyledProperty<IBrush?> HeaderBackgroundProperty =
+        AvaloniaProperty.Register<MultiSelect, IBrush?>(nameof(HeaderBackground));
+
+    public IBrush? HeaderBackground
+    {
+        get { return GetValue(HeaderBackgroundProperty); }
+        set { SetValue(HeaderBackgroundProperty, value); }
     }
     
     public MultiSelect()
