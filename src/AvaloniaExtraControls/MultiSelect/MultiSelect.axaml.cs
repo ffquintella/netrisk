9c5be2795b51b83728b8f154c03147bdb7a7430a
Felipe F Quintella <felipe@quintella.com>
2023-08-11 21:21:08 -0300

    inc

--- src/AvaloniaExtraControls/MultiSelect/MultiSelect.axaml.cs
+++ src/AvaloniaExtraControls/MultiSelect/MultiSelect.axaml.cs
@@ -1,8 +1,11 @@
-﻿using Avalonia;
+﻿using System.Collections;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
+using ReactiveUI;
+using System.Reactive;
 
 namespace AvaloniaExtraControls.MultiSelect;
 
@@ -35,14 +38,95 @@ public class MultiSelect : TemplatedControl
         set { SetValue(TitleProperty, value); }
     }
     
+    public static readonly StyledProperty<IEnumerable?> ItemsSourceProperty =
+        AvaloniaProperty.Register<MultiSelect, IEnumerable?>(nameof(ItemsSource));
+
+    public IEnumerable? ItemsSource
+    {
+        get { return GetValue(ItemsSourceProperty); }
+        set
+        {
+            SetValue(SelectedSelectedItemsProperty, null);
+            SetValue(AvailableItemsProperty, value);
+            
+            SetValue(ItemsSourceProperty, value);
+        }
+    }
+    
+    public static readonly StyledProperty<IEnumerable?> AvailableItemsProperty =
+        AvaloniaProperty.Register<MultiSelect, IEnumerable?>(nameof(AvailableItems));
+
+    public IEnumerable? AvailableItems
+    {
+        get { return GetValue(AvailableItemsProperty); }
+        set { SetValue(AvailableItemsProperty, value); }
+    }
+    
+    public static readonly StyledProperty<IEnumerable?> SelectedAvailableItemsProperty =
+        AvaloniaProperty.Register<MultiSelect, IEnumerable?>(nameof(SelectedAvailableItems));
+
+    public IEnumerable? SelectedAvailableItems
+    {
+        get { return GetValue(SelectedAvailableItemsProperty); }
+        set { SetValue(SelectedAvailableItemsProperty, value); }
+    }
+    
+    
+    public static readonly StyledProperty<IEnumerable?> SelectedItemsProperty =
+        AvaloniaProperty.Register<MultiSelect, IEnumerable?>(nameof(SelectedItems));
+
+    public IEnumerable? SelectedItems
+    {
+        get { return GetValue(SelectedItemsProperty); }
+        set { SetValue(SelectedItemsProperty, value); }
+    }
+    
+    public static readonly StyledProperty<IEnumerable?> SelectedSelectedItemsProperty =
+        AvaloniaProperty.Register<MultiSelect, IEnumerable?>(nameof(SelectedSelectedItems));
+
+    public IEnumerable? SelectedSelectedItems
+    {
+        get { return GetValue(SelectedSelectedItemsProperty); }
+        set { SetValue(SelectedSelectedItemsProperty, value); }
+    }
+    
+    public static readonly StyledProperty<IReactiveCommand?> BtMoveRightClickedProperty =
+        AvaloniaProperty.Register<MultiSelect, IReactiveCommand?>(nameof(BtMoveRightClicked));
+    
+    public IReactiveCommand? BtMoveRightClicked 
+    {
+        get { return GetValue(BtMoveRightClickedProperty); }
+        set { SetValue(BtMoveRightClickedProperty, value); }
+    }
+    
+    public static readonly StyledProperty<IReactiveCommand?> BtMoveLeftClickedProperty =
+        AvaloniaProperty.Register<MultiSelect, IReactiveCommand?>(nameof(BtMoveLeftClicked));
+    public IReactiveCommand? BtMoveLeftClicked     
+    {
+        get { return GetValue(BtMoveLeftClickedProperty); }
+        set { SetValue(BtMoveLeftClickedProperty, value); }
+    }
+    
     public MultiSelect()
     {
+        BtMoveRightClicked = ReactiveCommand.Create(ExecuteMoveRight);
+        BtMoveLeftClicked = ReactiveCommand.Create(ExecuteMoveLeft);
+        
         InitializeComponent();
+    }
+
+    private void ExecuteMoveRight()
+    {
+        
+    }
+    
+    private void ExecuteMoveLeft()
+    {
+        
     }
     
     private void InitializeComponent()
     {
         
-     
     }
 }
