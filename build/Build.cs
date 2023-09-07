f5367cec1853d0cdf4ecda0710d149c407f30802
Felipe F Quintella <felipe@quintella.com>
2023-09-07 11:22:28 -0300

    new build project

---
+++ build/Build.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using Serilog;
+
+class Build : NukeBuild
+{
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    Target Print => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            Log.Information("STARTING BUILD");
+        });
+    
+    Target Clean => _ => _
+        .Before(Restore)
+        .DependsOn(Print)
+        .Executes(() =>
+        {
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .DependsOn(Print)
+        .Executes(() =>
+        {
+        });
+
+}
