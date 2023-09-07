7257b8856d06a469b920b1dc20a3f6fb7a7a5376
Felipe F Quintella <felipe@quintella.com>
2023-09-07 11:11:39 -0300

    Cleanning old build system

--- src/build/CloudsmithResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DefaultNamespace;
-
-public class CloudsmithResponse
-{
-    public string Identifier { get; set; } = "";
-    public string Etag { get; set; } = "";
-    public string Detail { get; set; } = "";
-}
