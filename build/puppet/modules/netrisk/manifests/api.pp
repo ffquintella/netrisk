db8520d0ef475f700aefebce00328bf32226e397
Felipe F Quintella <felipe@quintella.com>
2023-09-10 17:14:50 -0300

    INC

---
+++ build/puppet/modules/netrisk/manifests/api.pp
@@ -0,0 +1,64 @@
+# == Class: srnet::params
+#
+# Defines default values for srnet module
+#
+class srnet::api (
+  # Database Settings
+  $dbserver   = netrisk::params::dbserver,
+  $dbuser     = netrisk::params::dbuser,
+  $dbport     = netrisk::params::dbport,
+  $dbpassword = netrisk::params::dbpassword,
+  $dbschema   = netrisk::params::dbschema,
+
+  #SAML Settings
+  $enable_saml       = netrisk::params::enable_saml,
+  $idp_entity_id     = netrisk::params::idp_entity_id,
+  $idp_name          = netrisk::params::idp_name,
+  $idp_sso_service   = netrisk::params::idp_sso_service,
+  $idp_ssout_service = netrisk::params::idp_ssout_service,
+  $idp_artifact_resolve_srvc = netrisk::params::idp_artifact_resolve_srvc,
+  $idp_certificate_file      = netrisk::params::idp_certificate_file,
+  $sp_certificate_file = netrisk::params::sp_certificate_file,
+  $sp_certificate_pwd  = netrisk::params::sp_certificate_pwd,
+
+  #Server
+  $server_logging          = netrisk::params::server_logging,
+  $server_https_port       = netrisk::params::server_https_port,
+  $server_certificate_file = netrisk::params::server_certificate_file,
+  $server_certificate_pwd  = netrisk::params::server_certificate_pwd,
+) inherits netrisk::params  {
+
+  file{'/netrisk/appsettings.json':
+    ensure  => file,
+    content => epp('netrisk/api/appsettings.json.epp', {
+      'server_url'     => $srnet_url,
+      'enable_saml'    => $enable_saml,
+      'server_logging' => $server_logging,
+      'sp_certificate_file'   => $sp_certificate_file,
+      'sp_certificate_pwd'    => $sp_certificate_pwd,
+      'idp_entity_id'             => $idp_entity_id,
+      'idp_name'                  => $idp_name,
+      'idp_sso_service'           => $idp_sso_service,
+      'idp_ssout_service'         => $idp_ssout_service,
+      'idp_artifact_resolve_srvc' => $idp_artifact_resolve_srvc,
+      'idp_certificate_file'      => $idp_certificate_file,
+      'db_server'   => $dbserver,
+      'db_user'     => $dbuser,
+      'db_port'     => $dbport ,
+      'db_password' => $dbpw_fin ,
+      'db_schema'   => $dbschema,
+      'server_https_port'       => $server_https_port,
+      'server_certificate_file' => $server_certificate_file,
+      'server_certificate_pwd'  => $server_certificate_pwd
+    })
+  }
+
+  exec{'Starting NetRisk API Server':
+    cwd         => '/netrisk/',
+    command     => '/netrisk/API &',
+    environment => ['ASPNETCORE_ENVIRONMENT=production','DOTNET_USER_SECRETS_FALLBACK_DIR=/tmp'],
+    user        => root,
+    logoutput   => true
+  }
+
+}
--- build/puppet/modules/netrisk/manifests/init.pp
+++ build/puppet/modules/netrisk/manifests/init.pp
@@ -1,55 +1,31 @@
 class netrisk (
-  # SRNet Settings
+  # NetRisk Settings
   $netrisk_url     = '',
 
-  # Database Settings
-  $dbserver   = '127.0.0.1',
-  $dbuser     = 'simplerisk',
-  $dbport     = '3306',
-  $dbpassword = '',
-  $dbschema   = 'simplerisk',
 
-  #SAML Settings
-  $enable_saml       = false,
-  $idp_entity_id     = 'https://stubidp.sustainsys.com',
-  $idp_name          = 'stubidp.sustainsys',
-  $idp_sso_service   = 'https://stubidp.sustainsys.com/',
-  $idp_ssout_service = 'https://stubidp.sustainsys.com/Logout',
-  $idp_artifact_resolve_srvc = 'https://stubidp.sustainsys.com/ArtifactResolve',
-  $idp_certificate_file      = 'Certificates/stubidp.sustainsys.com.cer',
-  $sp_certificate_file = 'Certificates/demowebapp.local.pfx',
-  $sp_certificate_pwd  = 'pass',
-
-  #Server
-  $server_logging          = 'Information',
-  $server_https_port       = 5443,
-  $server_certificate_file = "Certificates/certificate.pfx",
-  $server_certificate_pwd  = "pass"
-
-) inherits srnet::params {
+) inherits netrisk::params {
 
 # UPDATE THIS EVERY RELEASE
-$srnetmaxdbver = 2
+#$nriskmaxdbver = 2
 
 #$dbpwd = String(file('/passwords/pass_netrisk.txt'), "%t")
-#$srnetdbver = String(file('/configurations/srnetdb.version'), "%t")
+#$nriskdbver = String(file('/configurations/srnetdb.version'), "%t")
+#$n_srvdbver = 0 + $srnetdbver
 
-$n_srvdbver = 0 + $srnetdbver
+#if ( $dbpassword == '') {
+#  $dbpw_fin = $dbpwd
+#}else{
+#  $dbpw_fin = $dbpassword
+#}
 
-if ( $dbpassword == '') {
-  $dbpw_fin = $dbpwd
-}else{
-  $dbpw_fin = $dbpassword
-}
-
-if($n_srvdbver != $srnetmaxdbver) {
-  Integer[$n_srvdbver, $srnetmaxdbver].each |$x| {
+#if($n_srvdbver != $srnetmaxdbver) {
+#  Integer[$n_srvdbver, $srnetmaxdbver].each |$x| {
     #notice("updating DB version ${x}")
-    exec{"updating DB version ${x}":
-      command => "/bin/bash -c 'MYSQL_PWD=${dbpw_fin} mysql -u${dbuser} -e \"use netrisk; \\. /scripts/netrisk-db/DB-SQL-${x}.sql\" && echo ${x} > /configurations/netriskdb.version'"
-    }
-  }
-}
+#    exec{"updating DB version ${x}":
+#      command => "/bin/bash -c 'MYSQL_PWD=${dbpw_fin} mysql -u${dbuser} -e \"use netrisk; \\. /scripts/netrisk-db/DB-SQL-${x}.sql\" && echo ${x} > /configurations/netriskdb.version'"
+#    }
+#  }
+#}
 
 
 file{'/srnet/SRNET-ConsoleClient/appsettings.json': 
@@ -84,30 +60,7 @@ file{'/srnet/SRNET-GUIClient-osx/appsettings.json':
   })
 }
 
-file{'/srnet/SRNET-Server/appsettings.json': 
-  ensure  => file,
-  content => epp('srnet/server/appsettings.json.epp', {
-    'server_url'     => $srnet_url,
-    'enable_saml'    => $enable_saml,
-    'server_logging' => $server_logging,
-    'sp_certificate_file'   => $sp_certificate_file,
-    'sp_certificate_pwd'    => $sp_certificate_pwd,
-    'idp_entity_id'             => $idp_entity_id,
-    'idp_name'                  => $idp_name,
-    'idp_sso_service'           => $idp_sso_service,
-    'idp_ssout_service'         => $idp_ssout_service,
-    'idp_artifact_resolve_srvc' => $idp_artifact_resolve_srvc,
-    'idp_certificate_file'      => $idp_certificate_file,
-    'db_server'   => $dbserver,
-    'db_user'     => $dbuser,
-    'db_port'     => $dbport ,
-    'db_password' => $dbpw_fin ,
-    'db_schema'   => $dbschema,
-    'server_https_port'       => $server_https_port,
-    'server_certificate_file' => $server_certificate_file,
-    'server_certificate_pwd'  => $server_certificate_pwd
-  })
-}
+
 
 # Compressing the pagages 
 
