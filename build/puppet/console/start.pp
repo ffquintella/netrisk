9896cd4b6e8b241ce6415504a81cc367f7b1d81f
Felipe F Quiontella <felipe@quintella.com>
2023-09-12 15:21:31 -0300

    INC

--- build/puppet/console/start.pp
+++ build/puppet/console/start.pp
@@ -15,29 +15,15 @@ if $dbschema == undef {
   fail('dbschema is not defined')
 }
 
-if $server_certificate_file == undef {
-  # This file will do the initial configuration of netrisk and start the service
-  class { 'netrisk::console':
-    netrisk_url       => $netrisk_url,
-    dbserver          => $dbserver,
-    dbuser            => $dbuser,
-    dbport            => $dbport,
-    dbpassword        => $dbpassword,
-    dbschema          => $dbschema,
-    server_logging    => $server_logging,
-    server_https_port => 0 + $server_https_port
-  }
-}else{
-  class { 'netrisk::console':
-    netrisk_url       => $netrisk_url,
-    dbserver          => $dbserver,
-    dbuser            => $dbuser,
-    dbport            => $dbport,
-    dbpassword        => $dbpassword,
-    dbschema          => $dbschema,
-    server_logging    => $server_logging,
-    server_https_port => 0 + $server_https_port,
-    server_certificate_file => $server_certificate_file,
-    server_certificate_pwd  => $server_certificate_pwd
-  }
+
+# This file will do the initial configuration of netrisk and start the service
+class { 'netrisk::console':
+  dbserver          => $dbserver,
+  dbuser            => $dbuser,
+  dbport            => $dbport,
+  dbpassword        => $dbpassword,
+  dbschema          => $dbschema,
+  server_logging    => $server_logging,
+
 }
+
--- build/puppet/modules/netrisk/manifests/console.pp
+++ build/puppet/modules/netrisk/manifests/console.pp
@@ -3,7 +3,7 @@
 # Defines default values for srnet module
 #
 class netrisk::console (
-  $netrisk_url = $netrisk::params::netrisk_url,
+  $server_logging = $netrisk::params::server_logging,
   
   # Database Settings
   $dbserver   = $netrisk::params::dbserver,
@@ -11,12 +11,6 @@ class netrisk::console (
   $dbport     = $netrisk::params::dbport,
   $dbpassword = $netrisk::params::dbpassword,
   $dbschema   = $netrisk::params::dbschema,
-
-  #Server
-  $server_logging          = $netrisk::params::server_logging,
-  $server_https_port       = $netrisk::params::server_https_port,
-  $server_certificate_file = $netrisk::params::server_certificate_file,
-  $server_certificate_pwd  = $netrisk::params::server_certificate_pwd,
   
 
   
@@ -25,27 +19,23 @@ class netrisk::console (
   file{'/netrisk/appsettings.json':
     ensure  => file,
     content => epp('netrisk/console/appsettings.json.epp', {
-      'server_url'     => $netrisk_url,
       'server_logging' => $server_logging,
       'db_server'   => $dbserver,
       'db_user'     => $dbuser,
       'db_port'     => $dbport ,
       'db_password' => $dbpassword ,
-      'db_schema'   => $dbschema,
-      'server_https_port'       => $server_https_port,
-      'server_certificate_file' => $server_certificate_file,
-      'server_certificate_pwd'  => $server_certificate_pwd
+      'db_schema'   => $dbschema
     })
   }
 
 
 
-  exec{'Starting NetRisk Website Server':
+  /*exec{'Starting NetRisk Website Server':
     cwd         => '/netrisk/',
     command     => '/netrisk/WebSite &',
     environment => ['ASPNETCORE_ENVIRONMENT=production','DOTNET_USER_SECRETS_FALLBACK_DIR=/tmp'],
     user        => root,
     logoutput   => true
-  }
+  }*/
 
 }
