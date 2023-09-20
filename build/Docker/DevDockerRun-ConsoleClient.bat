aac5e423806b624443eb170dc44810855c1e7b8b
Felipe F Quintella <felipe@quintella.com>
2023-09-20 20:18:42 -0300

    build & test

--- build/Docker/DevDockerRun-ConsoleClient.bat
+++ build/Docker/DevDockerRun-ConsoleClient.bat
@@ -1,2 +1,2 @@
 @echo off
-docker.exe run --env-file=dev-env-website.list --rm --entrypoint /bin/bash -ti ffquintella/netrisk-console:0.50.1 
+docker.exe run --env-file=dev-env-console.list --rm --entrypoint /bin/bash -ti ffquintella/netrisk-console:0.50.1 
