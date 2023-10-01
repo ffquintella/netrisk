451be09386e4ab313ab2cac2ab3c7bfe6240ca3b
Felipe F Quintella <felipe@quintella.com>
2023-10-01 19:13:06 -0300

    INC

---
+++ src/API/Controllers/HostsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ServerServices.Interfaces;
+using ILogger = Serilog.ILogger;
+
+namespace API.Controllers;
+
+
+[Authorize(Policy = "RequireValidUser")]
+[ApiController]
+[Route("[controller]")]
+public class HostsController: ApiBaseController
+{
+    public HostsController(ILogger logger, IHttpContextAccessor httpContextAccessor, IUsersService usersService) 
+        : base(logger, httpContextAccessor, usersService)
+    {
+    }
+    
+    
+}
