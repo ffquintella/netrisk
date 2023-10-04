b655219b38d6e4826e41cf2aafd798b16bbe37eb
Felipe F Quintella <felipe@quintella.com>
2023-10-04 17:47:32 -0300

    fix

--- src/ConsoleClient/DB/Structure/5.sql
+++ src/ConsoleClient/DB/Structure/5.sql
@@ -2,3 +2,7 @@
 ALTER TABLE `risks`
     ADD CONSTRAINT `fk_risk_category` FOREIGN KEY (`category`) REFERENCES `category` (`value`) ON DELETE RESTRICT,
     ADD CONSTRAINT `fk_risk_source` FOREIGN KEY (`source`) REFERENCES `source` (`value`) ON DELETE RESTRICT;
+
+ALTER TABLE `risks`
+    MODIFY COLUMN `source` int(11) NULL AFTER `control_number`,
+    MODIFY COLUMN `category` int(11) NULL AFTER `source`;
