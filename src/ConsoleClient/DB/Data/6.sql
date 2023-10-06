cb0aa52be15921d6d0ea0116ba4228d85f49ee60
Felipe F Quintella <felipe@quintella.com>
2023-10-06 16:10:09 -0300

    technology data

---
+++ src/ConsoleClient/DB/Data/6.sql
@@ -0,0 +1,38 @@
+﻿-- ----------------------------
+-- Records of technology
+-- ----------------------------
+INSERT INTO `technology` VALUES (1, 'All');
+INSERT INTO `technology` VALUES (2, 'Anti-Virus');
+INSERT INTO `technology` VALUES (3, 'Backups');
+INSERT INTO `technology` VALUES (4, 'Smartphone');
+INSERT INTO `technology` VALUES (5, 'Switches');
+INSERT INTO `technology` VALUES (6, 'Datacenter');
+INSERT INTO `technology` VALUES (7, 'E-mail');
+INSERT INTO `technology` VALUES (8, 'Realtime colaboration');
+INSERT INTO `technology` VALUES (9, 'Messages');
+INSERT INTO `technology` VALUES (10, 'Mobile Device');
+INSERT INTO `technology` VALUES (11, 'Network');
+INSERT INTO `technology` VALUES (12, 'Energy');
+INSERT INTO `technology` VALUES (13, 'Remote acess');
+INSERT INTO `technology` VALUES (14, 'File Server');
+INSERT INTO `technology` VALUES (15, 'Telephony');
+INSERT INTO `technology` VALUES (16, 'Unix');
+INSERT INTO `technology` VALUES (17, 'Virtualization');
+INSERT INTO `technology` VALUES (18, 'Web');
+INSERT INTO `technology` VALUES (19, 'Windows');
+INSERT INTO `technology` VALUES (20, 'Business Systems');
+INSERT INTO `technology` VALUES (21, 'Active Directory');
+INSERT INTO `technology` VALUES (22, 'DBMS');
+INSERT INTO `technology` VALUES (23, 'Low Code');
+INSERT INTO `technology` VALUES (24, 'Legacy Systems');
+INSERT INTO `technology` VALUES (25, 'Web Portals');
+INSERT INTO `technology` VALUES (26, 'Monitoring Systems');
+INSERT INTO `technology` VALUES (27, 'Web Portals');
+INSERT INTO `technology` VALUES (28, 'PHP');
+INSERT INTO `technology` VALUES (29, 'Java');
+INSERT INTO `technology` VALUES (30, '.Net');
+INSERT INTO `technology` VALUES (31, 'Intranet');
+INSERT INTO `technology` VALUES (32, 'Firewalls');
+
+
+update settings SET value = '6' where name = 'db_version';
