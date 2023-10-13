bd90670132d62bb1e4f989f00660a0b7a30f89d8
Felipe F Quintella <felipe@quintella.com>
2023-10-12 22:56:01 -0300

    inc

--- src/ClientServices/Services/Importers/NessusImporter.cs
+++ src/ClientServices/Services/Importers/NessusImporter.cs
@@ -1,14 +1,48 @@
-﻿using ClientServices.Events;
+﻿using System.Xml.Linq;
+using ClientServices.Events;
 using ClientServices.Interfaces.Importers;
 
 namespace ClientServices.Services.Importers;
 
 public class NessusImporter: IVulnerabilityImporter
 {
-    public int Import(string filePath)
+    public async Task<int> Import(string filePath)
     {
         int importedVulnerabilities = 0;
 
+        if (!File.Exists(filePath)) throw new FileNotFoundException("File not found");
+        
+        var data = File.ReadAllText(filePath);
+        
+        XDocument doc = XDocument.Parse(data);
+        Dictionary<string, string> dataDictionary = new Dictionary<string, string>();
+
+        var descendants = doc.Descendants().Where(p => p.HasElements == false);
+
+        var tic = descendants.Count() / 100;
+
+        int interactions = 0;
+
+        await Task.Run(() =>
+        {
+            foreach (XElement element in descendants) {
+                int keyInt = 0;
+                string keyName = element.Name.LocalName;
+
+                while (dataDictionary.ContainsKey(keyName)) {
+                    keyName = element.Name.LocalName + "_" + keyInt++;
+                }
+
+                dataDictionary.Add(keyName, element.Value);
+                interactions++;
+                if(interactions % tic == 0)
+                    NotifyStepCompleted(new ProgressBarrEventArgs{Progess = interactions / tic});
+            }
+        });
+
+
+        var bc = dataDictionary;
+        
 
         return importedVulnerabilities;
     }
@@ -18,6 +52,7 @@ public class NessusImporter: IVulnerabilityImporter
         var pc = new ProgressBarrEventArgs {Progess = 1};
         NotifyStepCompleted(pc);
     }
+    
     
     public event EventHandler<ProgressBarrEventArgs>? StepCompleted;
     
