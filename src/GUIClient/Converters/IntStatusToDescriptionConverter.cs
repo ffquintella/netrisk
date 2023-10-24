ba1d0853670be3c87f4f957a9a7480fc4231982c
Felipe Quintella <felipe@quintella.com>
2023-10-24 09:42:32 -0300

    status details

---
+++ src/GUIClient/Converters/IntStatusToDescriptionConverter.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Globalization;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using GUIClient.Extensions;
+using Model;
+
+namespace GUIClient.Converters;
+
+public class IntStatusToDescriptionConverter: IValueConverter
+{
+    public static readonly IntStatusToDescriptionConverter Instance = new();
+    
+    public object? Convert( object? value, 
+        Type targetType, 
+        object? parameter, 
+        CultureInfo culture)
+    {
+        if (value is null) return "";
+
+        int intVal = 0;
+        if(value is short) intVal = Int32.Parse(value!.ToString()!);
+        else if (value is ushort) intVal = Int32.Parse(value!.ToString()!);
+        else if (value is int) intVal = Int32.Parse(value!.ToString()!);
+        else if (value is long) intVal = Int32.Parse(value!.ToString()!);
+        else if (value is IntStatus) intVal = (int)value;
+        else throw new InvalidCastException();
+        
+
+        var intEnum = (IntStatus) Enum.ToObject(typeof(IntStatus), intVal);
+        return $"({intVal}) - {intEnum.StatusString()}";
+        
+        // converter used for the wrong type
+        return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
+    }
+    
+    public object ConvertBack( object? value, Type targetType, object? parameter, CultureInfo culture )
+    {
+        throw new NotSupportedException();
+    }
+}
