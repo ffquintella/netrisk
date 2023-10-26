9963625b0ef034a04a4563185abac5b1e60f627a
Felipe F Quiontella <felipe@quintella.com>
2023-10-26 10:44:30 -0300

    Fix close reasons

---
+++ src/ConsoleClient/DB/Data/11.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO `close_reason` (`value`, `name`) VALUES (0, 'Rejected');
+INSERT INTO `close_reason` (`value`, `name`) VALUES (1, 'Fully Mitigated');
+INSERT INTO `close_reason` (`value`, `name`) VALUES (2, 'Activity not donne anymore');
+INSERT INTO `close_reason` (`value`, `name`) VALUES (3, 'Belongs to third party');
+INSERT INTO `close_reason` (`value`, `name`) VALUES (4, 'Not relevant');
+
+
+update settings SET value = '11' where name = 'db_version';
