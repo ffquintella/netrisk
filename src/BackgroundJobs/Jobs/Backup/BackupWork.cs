a79ee0802e91f79ea37c1f14c62ac36280a1baee
Felipe F Quintella <felipe@quintella.com>
2023-11-12 16:21:42 -0300

    inc

---
+++ src/BackgroundJobs/Jobs/Backup/BackupWork.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using Serilog;
+using ServerServices.Services;
+
+namespace BackgroundJobs.Jobs.Backup;
+
+public class BackupWork:  BaseJob, IJob
+{
+     
+    DatabaseService DatabaseService { get; }
+     
+    public BackupWork(ILogger logger, DALService dalService, DatabaseService dbservice) : base(logger, dalService)
+    {
+        DatabaseService = dbservice;
+    }
+
+    public void Run()
+    {
+        try
+        {
+            DatabaseService.Backup();
+        }
+        catch (Exception e)
+        {
+            
+            Log.Error("Error during backup: {Message}", e.Message);
+            throw;
+        }
+        
+    }
+}
