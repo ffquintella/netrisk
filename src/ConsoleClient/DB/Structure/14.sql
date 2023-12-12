3321a2fb1304a8bc172c674d4e989ecd3d1d8e94
Felipe Quintella <felipe@quintella.com>
2023-12-12 10:28:55 -0300

    inc

--- src/ConsoleClient/DB/Structure/14.sql
+++ src/ConsoleClient/DB/Structure/14.sql
@@ -3,4 +3,3 @@ ALTER TABLE `assessment_runs`
     ADD COLUMN `Status` int NOT NULL AFTER `AnalystId`,
     ADD INDEX `idxStatus`(`Status`) USING BTREE;
 
-ALTER TABLE `assessment_questions` DROP FOREIGN KEY `fk_assessment`;
