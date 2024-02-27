833a235886db6de6f12a0665b980cf4116c295e8
Felipe Quintella <felipe@quintella.com>
2024-02-27 13:39:29 -0300

    inc

--- src/GUIClient/ViewModels/Reports/EntitiesRisksViewModel.cs
+++ src/GUIClient/ViewModels/Reports/EntitiesRisksViewModel.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using ClientServices.Interfaces;
 using LiveChartsCore;
+using LiveChartsCore.Measure;
 using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using Model.Statistics;
 using ReactiveUI;
+using SkiaSharp;
 
 namespace GUIClient.ViewModels.Reports;
 
@@ -44,6 +51,12 @@ public class EntitiesRisksViewModel: ReportsViewModelBase
         }
     };
     #endregion
+    
+    #region SERVICES
+
+    private IStatisticsService StatisticsService { get; } = GetService<IStatisticsService>();
+    
+    #endregion
 
     #region CONSTRUCTOR
         public EntitiesRisksViewModel()
@@ -56,6 +69,40 @@ public class EntitiesRisksViewModel: ReportsViewModelBase
 
     public void ExecuteGenerate()
     {
+        var dataList = StatisticsService.GetEntitiesRiskValues();
+
+        var grouped = dataList.GroupBy(v => v.Type).ToList();
+
+        var labels = new List<string>();
+        
+        foreach (var group in grouped)
+        {
+            Series.Add(
+                new ColumnSeries<float>
+                {
+                    Name = group.Select(v => v.Name).FirstOrDefault(),
+                    Values = group.Select(v => v.Value)
+                });
+            if(!labels.Contains(group.Key))
+                labels.Add(group.Key);
+        }
+
+        XAxes = new[]
+        {
+            new Axis
+            {
+                Labels = labels.ToArray(),
+                LabelsRotation = 0,
+                SeparatorsPaint = new SolidColorPaint(new SKColor(200, 200, 200)),
+                SeparatorsAtCenter = false,
+                TicksPaint = new SolidColorPaint(new SKColor(35, 35, 35)),
+                TicksAtCenter = true,
+                ForceStepToMin = true,
+                MinStep = 1
+            }
+        };
+        
+
     }
 
     #endregion
