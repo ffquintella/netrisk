12f093cb9cd336e46e6906bcf04526e1ef10677e
Felipe Quintella <felipe@quintella.com>
2024-06-18 14:15:25 -0300

    inc

--- src/Model/DTO/FixResquestDto.cs
+++ src/Model/DTO/FixResquestDto.cs
@@ -3,7 +3,7 @@ namespace Model.DTO;
 public class FixResquestDto
 {
     int VulnerabilityId { get; set; }
-    string Comments { get; set; }
-    string Destination { get; set; }
+    private string Comments { get; set; } = "";
+    private string Destination { get; set; } = "";
     int? FixTeamId { get; set; }
 }
