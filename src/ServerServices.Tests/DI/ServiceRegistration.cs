3c44f842270073c4a51cf1d046a618c0198599de
Felipe Quintella <felipe@quintella.com>
2024-07-02 16:52:28 -0300

    Inc

--- src/ServerServices.Tests/DI/ServiceRegistration.cs
+++ src/ServerServices.Tests/DI/ServiceRegistration.cs
@@ -42,6 +42,7 @@ public class ServiceRegistration
         services.AddTransient<IClientRegistrationService, ClientRegistrationService>();
         services.AddTransient<IUsersService, UsersService>();
         services.AddTransient<IPermissionsService, PermissionsService>();
+        services.AddTransient<IMessagesService, MessagesService>();
         services.AddScoped<ISieveProcessor, ApplicationSieveProcessor>();
         
         services.AddSingleton<ILocalizationService>(new LocalizationService(factory, typeof(ApplicationSieveProcessor).Assembly));
