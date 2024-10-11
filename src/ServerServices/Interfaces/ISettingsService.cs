b4c43ec1423b25e0b5ac360789ffaff48e8a9e25
Felipe F Quintella <felipe@quintella.com>
2024-10-10 22:45:06 -0300

    inc

--- src/ServerServices/Interfaces/ISettingsService.cs
+++ src/ServerServices/Interfaces/ISettingsService.cs
@@ -2,5 +2,9 @@ namespace ServerServices.Interfaces;
 
 public interface ISettingsService
 {
-    
+    /// <summary>
+    /// Change the backup password
+    /// </summary>
+    /// <param name="password"></param>
+    public void ChangeBackupPasswordAsync(string password);
 }
--- src/ServerServices/Services/SettingsService.cs
+++ src/ServerServices/Services/SettingsService.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Model.Exceptions;
 using Serilog;
 using ServerServices.Interfaces;
 
@@ -5,5 +7,26 @@ namespace ServerServices.Services;
 
 public class SettingsService(ILogger logger, IDalService dalService):  ServiceBase(logger, dalService), ISettingsService
 {
-    
+    public async void ChangeBackupPasswordAsync(string password)
+    {
+        if (password == null || password == "")
+        {
+            Logger.Error("Please enter the password!");
+            throw new InvalidParameterException("password", "Please enter the password!");
+        }
+        
+        // Change the password
+        await using var dbContext = DalService.GetContext();
+        
+        var bkp_pwd  = await dbContext.Settings.FirstOrDefaultAsync(x => x.Name == "BackupPassword");
+
+        if (bkp_pwd == null)
+        {
+            Log.Error("Backup password not found in the database!");
+            return;
+        }
+        
+        bkp_pwd.Value = password;
+        await dbContext.SaveChangesAsync();
+    }
 }
