1e24aa8e8b344855fc816457fe7ca02ff67a9885
Felipe F Quintella <felipe@quintella.com>
2024-11-16 09:28:37 -0300

    inc

--- src/ConsoleClient/DB/Data/33.sql
+++ src/ConsoleClient/DB/Data/33.sql
@@ -1,7 +1,5 @@
 ﻿START TRANSACTION;
 
-CREATE FULLTEXT INDEX `idx_action_message` ON `nr_actions` (`Message`);
-
 INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
 VALUES ('20241116011937_newIndexActions', '8.0.10');
 
